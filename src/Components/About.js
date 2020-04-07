2da296bbb8fdd46316d788d53ee8a139edad0f42
Lily Tan <lilyt93@gmail.com>
2020-04-07 17:28:40 -0400

    added circular pic

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+
 export default class About extends React.Component {
   render() {
     return (
       <div>
+        <img src = 'https://media-exp1.licdn.com/dms/image/C4D03AQHJe5fSwQWCug/profile-displayphoto-shrink_200_200/0?e=1591833600&v=beta&t=LrjDncmbI4U-3aCwsEghjsgopDALj6mk0vvqWj-C7a4' alt = 'profilepic' class = 'profile'></img>
       <p>
         Hi!  My name is Lily and I'm from Brooklyn, New York.  I studied biology in college at Stony Brook University because I was always curious about how things worked.  That curiousity didn't go away after college, and during my time at real estate startup, I wanted to know in more detail what it meant to create software - so I joined Grace Hopper Academy.
       </p>
