4dc20f121955c0ca515c757cc7f64f9033aa7ce1
Lily Tan <lilyt93@gmail.com>
2020-05-26 15:03:18 -0400

    project card updates

--- src/Projects.js
+++ src/Projects.js
@@ -17,23 +17,38 @@ export default class Projects extends React.Component {
       <div class = 'cardHolder'>
       <div class = 'card'>
         <h1>[Hop Hop] Array</h1>
-        <p>An approachable platform where users can practice completing algorithms.</p>
-        <p>Made with Javascript, React, Redux, Styled Components, Docker, Express, Postgres</p>
+        <p>An approachable platform where users can practice algorithms.</p>
+        <p>Made with Javascript, React, Redux, Docker, Express, Postgres</p>
         <div class = 'buttonHolder'>
         <p><a href = "https://github.com/hophoparray/hop_hop_array">Github</a></p>
       <p><a href = "https://www.hophoparray.com">Deployed Site</a></p>
       </div>
       </div>
+
       <div class = 'card'>
       <h1>Made Tiny</h1>
       <p>A mock e-commerce site used to sell tiny houses.</p>
-      <p>Made with Javascript, React, Redux, Express, Postgres</p>
+      <p>Made with Javascript, React, Redux, Express, Postgres
+      </p>
       <div class = 'buttonHolder'>
       <p><a href = "https://github.com/1911-grace-shopper/grace-shopper">Github</a></p>
       <p><a href = "https://madetiny.herokuapp.com">Deployed Site</a></p>
       </div>
     </div>
+
+    <div class = 'card'>
+      <h1>Mafia</h1>
+      <p>An online version of the game Mafia.</p>
+      <p>Made with Javascript, React, Redux, Express, Postgres
+
+      </p>
+      <div class = 'buttonHolder'>
+      <p><a href = "https://github.com/LTan9876/mafia">Github</a></p>
+      <p><a href = " ">Not Deployed</a></p>
+      </div>
     </div>
+    </div>
+
     )
   }
 }
